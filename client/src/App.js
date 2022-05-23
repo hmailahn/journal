17771bcf3fda357f1c63b138edb6ffc4c0366b6a
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-05-22 20:53:32 -0500

    added bootstrap for styling rn

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// import Profile from './pages/Profile';
+
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ApolloProvider client={client}>
+    <div className='flex-column justify-flex-start min-100-vh'>
+      <Header />
+      <div className='container'>
+      </div>
+      <Footer />
     </div>
+        </ApolloProvider>
   );
 }
 
